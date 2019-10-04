2d711d536ef54bf8e0d234b0f7c6b16f559aadbc
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-04 22:38:18 +0000

    Initial Commit

---
+++ HAROLD.py
@@ -0,0 +1,29 @@
+import os
+import time
+import RPi.GPIO as GPIO
+
+os.system('modprobe wire timeout=1 slave_ttl=5')
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-smem')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_slaves')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_remove')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_search')
+base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
+delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
+
+def main():
+    while True:
+        f = open(base_dir, "r")
+        ID = f.read()
+        f.close()
+        if != 'not found.\n'
+            print(ID)
+            break
+        else:
+            print("Waiting")
+
+        print("FINISHED")
+
+if __name__ == '__main__':
+    main()
+        
