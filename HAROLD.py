75b26e075d7d3781eb53f6f58d7235022ef889e7
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-04 22:49:12 +0000

    Fixed if statement

--- HAROLD.py
+++ HAROLD.py
@@ -16,7 +16,7 @@ def main():
         f = open(base_dir, "r")
         ID = f.read()
         f.close()
-        if != 'not found.\n'
+        if ID != 'not found.\n'
             print(ID)
             break
         else:
