483cdacc04ac79ee9f375aa4f2cefca9e5845d1e
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-05 21:31:49 +0000

    add Audiophiler

--- HAROLD.py
+++ HAROLD.py
@@ -1,6 +1,11 @@
 import os
 import time
+import csh_ldap
+import requests
+import pygame
 import RPi.GPIO as GPIO
+
+from HAROLD import config
 
 os.system('modprobe wire timeout=1 slave_ttl=5')
 os.system('modprobe w1-gpio')
@@ -11,6 +16,12 @@ os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_search')
 base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
 delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
 
+instance = csh_ldap.CSHLDAP(uid=nfatkhiyev,cn=users,cn=accounts,dc=csh,dc=rit,dc=edu, config.PASSWORD)
+
+HAROLD_AUTH = config.harold_auth
+
+pygame.mixer.init()
+
 def main():
     while True:
         f = open(base_dir, "r")
@@ -19,13 +30,38 @@ def main():
         time.sleep(1)
         if ID != 'not found.\n':
             print(ID)
-            d = open(delete_dir, "w")
-            d.write(ID)
             break
         else:
             print("Waiting")
 
+    gets3Link(getAudiophiler(getUID(ID)))
+    pygame.mixer.music.load("music.mp3")
+    pygame.mixer.music.play()
+    time.sleep(30)
+    pygame.mixer.music.stop()
+    deleteMusic()
     print("FINISHED")
+
+
+def getUID(iButtonCode):
+    user = instance.get_member_ibutton(iButtonCode)
+    UID = user.uid
+    return UID
+
+def getAudiophiler(UID):
+    getHaroldURL = "https://audiophiler.csh.rit.edu/get_harold/" + UID
+    params = urlencode({
+            'auth':HAROLD_AUTH,
+    })
+    s3Link = requests.post(getHaroldURL, params.encode('utf-8'))
+    return s3Link
+
+def gets3Link(link):
+    music = requests.get(link, allow_redirects=True)
+    open('music.mp3', 'wb').write(music.content)
+
+def deleteMusic():
+    os.remove("music.mp3")
 
 if __name__ == '__main__':
     main()
