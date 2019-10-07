04142ed7feff20a822767bcdf285259775be9c84
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-07 21:22:30 +0100

    Added comments to working verison of Harold PI

--- HAROLD.py
+++ HAROLD.py
@@ -5,11 +5,13 @@ import requests
 import pygame
 import RPi.GPIO as GPIO
 
-#from urllib import urlencode
+#import the config file
 import config
 
+#set config password
 sudoPassword = config.SUDO_PASSWORD
 
+#setup I-Button paths
 os.system('echo %s|sudo modprobe wire timeout=1 slave_ttl=5' % (sudoPassword))
 os.system('echo %s|sudo modprobe w1-gpio' % (sudoPassword))
 os.system('echo %s|sudo modprobe w1-smem' % (sudoPassword))
@@ -19,53 +21,73 @@ os.system('echo %s|sudo chmod a+w /sys/devices/w1_bus_master1/w1_master_search' 
 base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
 delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
 
+#create an instance
 instance = csh_ldap.CSHLDAP("uid=nfatkhiyev,cn=users,cn=accounts,dc=csh,dc=rit,dc=edu", config.PASSWORD)
 
+#authentication config file
 HAROLD_AUTH = config.harold_auth
 
 pygame.mixer.init()
 
+#main
 def main():
-    ID = ""
+    ID = " "
+    #keep the whole program running so it doesn't play one song and stop
     while True:
+        #while loop per song
         while True: 
+            #read the file and set the ID to the I-Button that was read 
             time.sleep(0.5)
             f = open(base_dir, "r")
-            ID = f.read()
+            ID = f.readline()
             time.sleep(0.1)
             f.close()
+            #if I-Button is found play music
             if ID != 'not found.\n':
                 print(ID)
-                d = open(delete_dir, "w")
-                d.write(ID)
+                pygame.mixer.music.load("scanComplete")
+                pygame.mixer.music.play()
+                time.sleep(3)
+                #add the found I-Button to the base directory
+                while True:
+                    f2 = open(base_dir, "r")
+                    test = f2.readline()
+                    f2.close()
+                    if test != 'not found.\n':
+                        d = open(delete_dir, "w")
+                        d.write(test)
+                        continue
+                    else:
+                        print("iButton read file is clean")
+                        break
                 break
             else:
                 print("Waiting")
 
+        #play the music from the I-Button that currently exists in the base directory
         ID = "*" + ID[3:].strip() + "01"
-        print(ID)
         gets3Link(getAudiophiler(getUID(ID)))
         try:
             pygame.mixer.music.load("music")
             pygame.mixer.music.play()
-
             while True:
                 if pygame.mixer.music.get_busy() == False or pygame.mixer.music.get_pos()/1000 > 30:
                     break
             pygame.mixer.music.stop()
+            deleteMusic()
         except:
             os.system('vlc --stop-time 30 music --sout-al vlc://quit')
 
-        deleteMusic()
-        ID =""
+        ID = " "
         print("FINISHED")
 
-
+#getUID with the I-Button as an arg
 def getUID(iButtonCode):
     user = instance.get_member_ibutton(iButtonCode)
     UID = user.uid
     return UID
 
+#getAudiophiler with UID as an arg
 def getAudiophiler(UID):
     getHaroldURL = "https://audiophiler.csh.rit.edu/get_harold/" + UID
     params = {
@@ -84,6 +106,7 @@ def getAudiophiler(UID):
         defaultLink = requests.post(url = getDefaultURL, json = paramsD)
         return defaultLink.text
 
+#gets3Link with the Link as an arg
 def gets3Link(link):
     try:
         music = requests.get(link, allow_redirects=True)
@@ -92,9 +115,10 @@ def gets3Link(link):
         music = requests.get(getAudiophiler("nfatkhiyev"), allow_redirects=True)
         open('music', 'wb').write(music.content)
 
+#remove the music file from the directory
 def deleteMusic():
     os.remove("music")
 
+#run main
 if __name__ == '__main__':
     main()
-        
