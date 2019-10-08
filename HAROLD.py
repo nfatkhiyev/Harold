0038bc91ceea8611638c18f2b6a2a2dc097a10c0
Dr-N0 <beckettjenen@gmail.com>
2019-10-08 14:52:08 -0400

    Night mode

--- HAROLD.py
+++ HAROLD.py
@@ -1,13 +1,18 @@
+# main imports
 import os
 import time
 import csh_ldap
 import requests
 import pygame
 import RPi.GPIO as GPIO
+
+# import the config file
 import config
 
+# set config password
 sudoPassword = config.SUDO_PASSWORD
 
+# setup I-Button paths
 os.system('echo %s|sudo modprobe wire timeout=1 slave_ttl=5' % (sudoPassword))
 os.system('echo %s|sudo modprobe w1-gpio' % (sudoPassword))
 os.system('echo %s|sudo modprobe w1-smem' % (sudoPassword))
@@ -17,27 +22,41 @@ os.system('echo %s|sudo chmod a+w /sys/devices/w1_bus_master1/w1_master_search' 
 base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
 delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
 
+# create an instance
 instance = csh_ldap.CSHLDAP("uid=nfatkhiyev,cn=users,cn=accounts,dc=csh,dc=rit,dc=edu", config.PASSWORD)
 
+# authentication config file
 HAROLD_AUTH = config.harold_auth
 
 pygame.mixer.init()
 
+timeNow = time.localtime()
+
+# main function
 def main():
     ID = " "
-    songsPlayed = 0
+    volume = 0
+    # keep the whole program running so it doesn't play one song and stop
     while True:
+        if 15 >= timeNow.tm_hour >= 9:
+            volume = 0.7
+        else:
+            volume = 0.3
+        # while loop per song
         while True:
+            # read the file and set the ID to the I-Button that was read
             time.sleep(0.5)
             f = open(base_dir, "r")
             ID = f.readline()
             time.sleep(0.1)
             f.close()
+            # if I-Button is found play music
             if ID != 'not found.\n':
                 print(ID)
                 pygame.mixer.music.load("scanComplete")
                 pygame.mixer.music.play()
-                time.sleep(3)
+                pygame.mixer.music.set_volume(volume)
+                # add the found I-Button to the base directory
                 while True:
                     f2 = open(base_dir, "r")
                     test = f2.readline()
@@ -53,46 +72,59 @@ def main():
             else:
                 print("Waiting")
 
+        # play the music from the I-Button that currently exists in the base directory
         ID = "*" + ID[3:].strip() + "01"
         gets3Link(getAudiophiler(getUID(ID)))
+        # try to play music and if you can't play the music then quit the vlc process
         try:
+            # load the music
             pygame.mixer.music.load("music")
             pygame.mixer.music.play()
-
+            pygame.mixer.music.set_volume(volume)
+            # play the music for thirty seconds
             while True:
-                if pygame.mixer.music.get_busy() == False or pygame.mixer.music.get_pos()/1000 > 30:
+                if pygame.mixer.music.get_busy() == False or pygame.mixer.music.get_pos() / 1000 > 30:
                     break
+            # stop the music
             pygame.mixer.music.stop()
+            # delete the music file from the root directory
             deleteMusic()
         except:
             os.system('vlc --stop-time 30 music --sout-al vlc://quit')
 
-        ID =""
+        # reset variables
+        ID = " "
         print("FINISHED")
 
+
+# getUID with the I-Button as an arg
 def getUID(iButtonCode):
     user = instance.get_member_ibutton(iButtonCode)
     UID = user.uid
     return UID
 
+
+# getAudiophiler with UID as an arg
 def getAudiophiler(UID):
     getHaroldURL = "https://audiophiler.csh.rit.edu/get_harold/" + UID
     params = {
-        'auth_key':HAROLD_AUTH
+        'auth_key': HAROLD_AUTH
     }
     try:
-        s3Link = requests.post(url = getHaroldURL, json = params)
+        s3Link = requests.post(url=getHaroldURL, json=params)
         print(s3Link.text)
         return s3Link.text
     except:
         print(e)
         getDefaultURL = "https://audiophiler.csh.rit.edu/get_harold/nfatkhiyev"
         paramsD = {
-            'auth_key':HAROLD_AUTH
+            'auth_key': HAROLD_AUTH
         }
-        defaultLink = requests.post(url = getDefaultURL, json = paramsD)
+        defaultLink = requests.post(url=getDefaultURL, json=paramsD)
         return defaultLink.text
 
+
+# gets3Link with the Link as an arg
 def gets3Link(link):
     try:
         music = requests.get(link, allow_redirects=True)
@@ -101,8 +133,12 @@ def gets3Link(link):
         music = requests.get(getAudiophiler("nfatkhiyev"), allow_redirects=True)
         open('music', 'wb').write(music.content)
 
+
+# remove the music file from the directory
 def deleteMusic():
     os.remove("music")
 
+
+# run main
 if __name__ == '__main__':
     main()
