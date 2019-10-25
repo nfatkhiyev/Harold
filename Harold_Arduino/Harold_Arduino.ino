fcf00e74d40646d33474edfad78d3d5ee093fe8a
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-25 19:24:23 -0400

    Arduino OneWire Fix

--- Harold_Arduino/Harold_Arduino.ino
+++ Harold_Arduino/Harold_Arduino.ino
@@ -1,58 +1,74 @@
+//iButton uses OneWire
 #include <OneWire.h>
 
-
-OneWire ds(12);
+//iButton Variables
+OneWire  ds(7);
 byte addr[8];
-String keyStatus="";
+int but[6] = {0,149,107,48,13,0};
+String keyStatus = "";
 
-
-void setup(void) {
-  digitalWrite(11, HIGH);
+void setup()  
+{
+  //Set up RFID
   Serial.begin(9600);
+  //Set up iButton
+  pinMode(13, OUTPUT);
+  //Print a ready message
+  Serial.println("ready");
 }
 
+//The char in iButton
+char c;
 
-void loop(void) {
-  if (digitalRead(11) == LOW) {
-    digitalWrite(11,HIGH);
-  }
-  getKeyCode();
-  if(keyStatus=="ok"){
-      byte i;
-      for( i = 5; i>0; i--) {
-           //Serial.print(":");
-           Serial.print(addr[i], HEX);           
-      }
-      Serial.print('\n');
-      delay(3000);
-  }
-  else if (keyStatus!="") { Serial.print(keyStatus);}
+void loop(){
 
+  //Checks OneWire for iButton info
+  checkiButton();
   
-  delay(1000);
 }
 
+void checkiButton() {
+  
+  //Get info on the key
+  getKeyCode();
+  //if the key is good go through this
+  if(keyStatus=="ok"){
+      int i;
+      //For CSH iButtons there are 7 code segments in hexidecimal 
+      for( i = 7; i >= 0; i--) {
+        
+        //Adds leading zeros to hex if only one character long
+        if( addr[i] < 0x10){ 
+        Serial.print("0");
+        }
+        
+        //Prints rest of hex
+       Serial.print(addr[i], HEX);
+       
+      }
+      
+       //Keep a delay so that it doesn't mistakenly read the same iButton more than once
+      delay(1000);
+      //Makes a new line
+      Serial.println();
+      
+  }
+  else if (keyStatus!="") {
+    Serial.print(keyStatus);
+  }
+  }
 
+//This does a lot of weird stuff about seeing if the data in the iButton is worth getting
 void getKeyCode(){
   byte present = 0;
   byte data[12];
   keyStatus="";
-
-  if (!ds.search(addr)) {
+  
+  if ( !ds.search(addr)) {
       ds.reset_search();
       return;
   }
-  
-  digitalWrite(11, LOW);
-  if ( OneWire::crc8( addr, 7) != addr[7]) {
-      keyStatus="CRC invalid";
-      return;
-  }
-  
-  if ( addr[0] != 0x01) {
-      keyStatus="not DS1990A";
-      return;
-  }
+
   keyStatus="ok";
   ds.reset();
 }
