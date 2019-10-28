31270e5ab379c8dd6476521f6bfcae70b376d8f8
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-10-28 16:20:30 -0400

    fixed indentation Audio Processing

--- AUDIO_PROCESSING.py
+++ AUDIO_PROCESSING.py
@@ -20,17 +20,17 @@ def get_beat_times():
     
     os.remove("musicA.wav")
 
-        double_beat_array = [beat_times_thirty[0]]
-        for beat in range(0,len(beat_times_thirty)-1):
-            average = (beat_times_thirty[beat]+beat_times_thirty[beat+1])/2
-            double_beat_array.append(average)
-            double_beat_array.append(beat_array[beat+1])
+    double_beat_array = [beat_times_thirty[0]]
+    for beat in range(0,len(beat_times_thirty)-1):
+        average = (beat_times_thirty[beat]+beat_times_thirty[beat+1])/2
+        double_beat_array.append(average)
+        double_beat_array.append(beat_array[beat+1])
 
-        quad_beat_array = [double_beat_array[0]]
-        for beat in range(0,len(double_beat_array)-1):
-            average = (double_beat_array[beat]+double_beat_array[beat+1])/2
-            quad_beat_array.append(average)
-            quad_beat_array.append(double_beat_array[beat+1])
+    quad_beat_array = [double_beat_array[0]]
+    for beat in range(0,len(double_beat_array)-1):
+        average = (double_beat_array[beat]+double_beat_array[beat+1])/2
+        quad_beat_array.append(average)
+        quad_beat_array.append(double_beat_array[beat+1])
 
     print(beat_times_thirty)
     
