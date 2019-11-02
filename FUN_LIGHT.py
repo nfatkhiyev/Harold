fd7e7cde5d62ed784f83a0eea594fde1ab062efc
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2019-11-02 10:37:36 -0400

    duck

--- FUN_LIGHT.py
+++ FUN_LIGHT.py
@@ -1,11 +1,18 @@
 import LIGHT_BAR
 import time
+from pygame import mixer
 
 LIGHT_BAR.setup_light_bar_gpio()
 
 LIGHT_BAR.reset()
 
+pygame.mixer.init()
+
+
 def main():
+    pygame.mixer.music.load('duck')
+    pygame.mixer.music.load.play()
+
     while True:
         LIGHT_BAR.set_light_bar(LIGHT_BAR.get_random_gpio_state(), LIGHT_BAR.get_random_gpio_state(), LIGHT_BAR.get_random_gpio_state())
         time.sleep(0.2)
